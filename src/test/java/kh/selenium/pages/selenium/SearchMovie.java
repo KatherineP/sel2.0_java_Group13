4a05b8aebb7fec105637fe7b40215d0b3ac358cd
KatherineP <gucolk@gmail.com>
2015-07-16 15:28:01 +0300

    SearchMovieUpdate5

--- src/test/java/kh/selenium/pages/selenium/SearchMovie.java
+++ src/test/java/kh/selenium/pages/selenium/SearchMovie.java
@@ -2,8 +2,11 @@ package kh.selenium.pages.selenium;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 
@@ -18,13 +21,21 @@ public class SearchMovie extends kh.selenium.pages.TestBase {
         login();
         WebElement searchField = driver.findElement(By.xpath("//div[@class='searchbox']/input[1]"));
         searchField.clear();
+
+        List<WebElement> moviesBefore = driver.findElements(By.xpath(".//*[@id='results']/a")); //считаем общее к-во фильмов до посика
+        System.out.println("Amount of movies before search: " + moviesBefore.size());
+
         searchField.sendKeys("movie43" + Keys.RETURN);
         driver.navigate().refresh();
         WebDriverWait wait = new WebDriverWait(driver, 30);
+
+        List<WebElement> moviesAfter = driver.findElements(By.xpath(".//*[@id='results']/a")); //считаем к-во фильмов после поиска
+        System.out.println("Amount of movies after search: "+ moviesAfter.size());
+
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='movie_cover']/div[@title ='movie43']")));
         //ниже сделаем проверку, что хотя бы один элемент с таким именем найден(первый)
         assertTrue(isElementPresent(By.xpath("//div[@class='movie_cover']/div[@title ='movie43']")));
-        driver.findElement(By.xpath("//div[@class='movie_cover']/div[@title ='movie43']")).click(); //тут проверим что на элемент можно кликнуть. А значит от ТОЧНО появился.
+
 
 
 
